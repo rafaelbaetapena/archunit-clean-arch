9363c01d9fe19946db2a5d4469f2d1e9a2b0dd05
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2022-03-30 17:39:27 -0300

    initial project

---
+++ src/test/java/com/rafaelbaetapena/archunit/ArchunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelbaetapena.archunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
