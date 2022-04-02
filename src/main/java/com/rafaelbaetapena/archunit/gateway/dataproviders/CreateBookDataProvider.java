3d83563c066868a4b3bca01503fdf179bbedd5da
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2022-04-02 11:53:07 -0300

    implementing create book

--- src/main/java/com/rafaelbaetapena/archunit/gateway/dataproviders/CreateBookDataProvider.java
+++ src/main/java/com/rafaelbaetapena/archunit/gateway/dataproviders/CreateBookDataProvider.java
@@ -4,20 +4,23 @@ import com.rafaelbaetapena.archunit.core.commands.CreateBookCommand;
 import com.rafaelbaetapena.archunit.core.events.BookCreatedEvent;
 import com.rafaelbaetapena.archunit.core.models.Book;
 import com.rafaelbaetapena.archunit.core.ports.CreateBookPort;
+import com.rafaelbaetapena.archunit.gateway.postgres.BookEntity;
+import com.rafaelbaetapena.archunit.gateway.postgres.BookRepository;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Component
 public class CreateBookDataProvider implements CreateBookPort {
+
+    private final BookRepository repository;
+
+    public CreateBookDataProvider(BookRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Book create(CreateBookCommand command) {
-        System.out.println(command);
-        return new Book(
-                UUID.randomUUID(),
-                command.getName(),
-                command.getPublishingCompany()
-        );
+        var bookCreated = repository.save(BookEntity.fromCommand(command));
+        return bookCreated.toModel();
     }
 
     @Override
