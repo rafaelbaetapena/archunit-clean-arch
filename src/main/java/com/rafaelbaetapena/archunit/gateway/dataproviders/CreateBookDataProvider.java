332cfe636a5c8bafe2262a5bbb3425fdff94b728
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2022-04-02 10:47:25 -0300

    implementing CreateBookDataProvider

--- src/main/java/com/rafaelbaetapena/archunit/gateway/dataproviders/CreateBookDataProvider.java
+++ src/main/java/com/rafaelbaetapena/archunit/gateway/dataproviders/CreateBookDataProvider.java
@@ -6,15 +6,22 @@ import com.rafaelbaetapena.archunit.core.models.Book;
 import com.rafaelbaetapena.archunit.core.ports.CreateBookPort;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class CreateBookDataProvider implements CreateBookPort {
     @Override
     public Book create(CreateBookCommand command) {
-        return null;
+        System.out.println(command);
+        return new Book(
+                UUID.randomUUID(),
+                command.getName(),
+                command.getPublishingCompany()
+        );
     }
 
     @Override
     public void sendEvent(BookCreatedEvent event) {
-
+        System.out.println(event);
     }
 }
